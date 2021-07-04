87d9135f638df1670fe21a23e48dffbc8125ed75
kimbi91 <subscribefan@gmail.com>
2021-07-04 12:14:04 +0200

    complete

---
+++ src/Controller/PqtController.php
@@ -0,0 +1,137 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Stock;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class PqtController extends AbstractController {
+    /**
+     * @Route("/", name="stock_list")
+     * @Method({"GET"})
+     */
+    public function index() {
+
+        $stocks= $this->getDoctrine()->getRepository(Stock::class)->findAll();
+
+        return $this->render('stocks/index.html.twig', array('stocks' => $stocks ));
+    }
+
+    /**
+     * @Route("/stock/new", name="new_stock")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+        $stock = new Stock();
+
+        $form = $this->createFormBuilder($stock)
+            ->add('title', TextType::class, array(
+                'label' => 'Forgalmi Rendszám',
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('size', TextType::class, array(
+                'label' => 'Méret',
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('cond', TextType::class, array(
+                'label' => 'Állapot',
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Módosítás',
+                'attr'  => array('btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $stock = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($stock);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('stock_list');
+        }
+
+        return $this->render('stocks/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+
+    /**
+     * @Route("/stock/edit/{id}", name="edit_stock")
+     * Method({"GET", "POST"})
+     */
+    public function edit(Request $request, $id) {
+        $stock = new Stock();
+        $stock= $this->getDoctrine()->getRepository(Stock::class)->find($id);
+
+        $form = $this->createFormBuilder($stock)
+            ->add('title', TextType::class, array(
+                'label' => 'Forgalmi Rendszám',
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('size', TextType::class, array(
+                'label' => 'Méret',
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('cond', TextType::class, array(
+                'label' => 'Állapot',
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Módosítás',
+                'attr'  => array('btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('stock_list');
+        }
+
+        return $this->render('stocks/edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/stock/{id}", name="stock_show")
+     */
+    public function show($id) {
+        $stock= $this->getDoctrine()->getRepository(Stock::class)->find($id);
+
+        return $this->render('stocks/show.html.twig', array('stock' => $stock));
+    }
+
+    /**
+     * @Route("/stock/delete/{id}")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id) {
+        $stock = $this->getDoctrine()->getRepository(Stock::class)->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($stock);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("stock_list");
+    }
+}
---
+++ src/Entity/Stock.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StockRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StockRepository::class)
+ */
+class Stock
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $size;
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $cond;
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    public function getCond()
+    {
+        return $this->cond;
+    }
+
+    public function setCond($cond)
+    {
+        $this->cond = $cond;
+    }
+
+}
